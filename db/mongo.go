28e5c3fdac61dfa21c839bb759289e87a167aa04
Yun Seongmin <ilcm96@gmail.com>
2020-06-19 15:22:17 +0900

    Change the way connect to the DB

--- db/mongo.go
+++ db/mongo.go
@@ -1,16 +1,12 @@
 package db
 
 import (
-	"context"
-	"time"
-
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func Client() *mongo.Client {
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	clientOptions := options.Client().ApplyURI("mongodb://root:ilcmgpro903@db.sub02111041190.generalvcn.oraclevcn.com:27017")
-	client, _ := mongo.Connect(ctx, clientOptions)
+	client, _ := mongo.NewClient(clientOptions)
 	return client
 }
