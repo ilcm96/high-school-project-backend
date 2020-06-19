cb7130a4e09e611023eaeb5cf0b5dc78867581ea
Yun Seongmin <ilcm96@gmail.com>
2020-06-19 23:52:36 +0900

    Fix mongodb client errror

--- db/mongo.go
+++ db/mongo.go
@@ -1,12 +1,14 @@
 package db
 
 import (
+	"context"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func Client() *mongo.Client {
 	clientOptions := options.Client().ApplyURI("mongodb://root:ilcmgpro903@db.sub02111041190.generalvcn.oraclevcn.com:27017")
-	client, _ := mongo.NewClient(clientOptions)
+	client, _ := mongo.Connect(context.TODO(), clientOptions)
 	return client
 }
