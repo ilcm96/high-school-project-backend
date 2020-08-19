636c8ad319cbe4f76cf9413174ad7aa436b93042
Yun Seongmin <ilcm96@gmail.com>
2020-08-20 00:03:04 +0900

    Check OS and return mongodb URI

--- internal/db/mongo.go
+++ internal/db/mongo.go
@@ -2,15 +2,22 @@ package db
 
 import (
 	"context"
+	"runtime"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+func mongodbURI() string {
+	os := runtime.GOOS
+	if os == "windows" {
+		return "mongodb://0.0.0.0:27017"
+	}
+	return "mongodb://root:root@db.sub02111041190.generalvcn.oraclevcn.com:27017"
+}
+
 func Client() *mongo.Client {
-	//clientOptions := options.Client().ApplyURI("mongodb://172.17.0.2:27017")
-	//clientOptions := options.Client().ApplyURI("mongodb://0.0.0.0:27017")
-	clientOptions := options.Client().ApplyURI("mongodb://root:ilcmgpro903@db.sub02111041190.generalvcn.oraclevcn.com:27017")
+	clientOptions := options.Client().ApplyURI(mongodbURI())
 	client, _ := mongo.Connect(context.TODO(), clientOptions)
 	return client
 }
